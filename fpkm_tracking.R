352e8694fc43f7358dfdcab8f1c420c61db64988
djgoldst <78706156+djgoldst@users.noreply.github.com>
2021-03-17 10:15:02 -0400

    Add files via upload

--- fpkm_tracking.R
+++ fpkm_tracking.R
@@ -1,9 +1,9 @@
 #Read in the genes.fpkm_tracking file 
 fpkm <- read.table("/projectnb/bf528/users/saxophone/project2/scripts/programmer/P0_1_cufflinks/genes.fpkm_tracking", header = TRUE, sep = "\t")
-#Filter out genes where FPKM<500
+#Filter out genes where FPKM<250
 fpkm <- fpkm[fpkm$FPKM>250,]
 #Create histogram of gene name vs. log(FPKM)
 par(mar=c(6,5,4,2))
 options(scipen=999)
-barplot(fpkm$FPKM,names.arg = fpkm$gene_short_name,main="Gene Expression - FPKM",ylab="log(FPKM)",ylim=c(100,10^6),log="y",las=2)
+barplot(fpkm$FPKM,names.arg = fpkm$gene_short_name,main="Gene Expression - FPKM",ylim=c(10,10^6),ylab="log(FPKM)",log="y",las=2)
 box(which="plot",lty="solid")
